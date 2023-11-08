8aee653664ff1bbd9ad695beb11740fb4aded4a2
Netceor <45135347+Netceor@users.noreply.github.com>
2023-11-08 17:13:25 +0800

    操作接口
    7个动画操作
    op_type 1-7 代表 "制作咖啡","倒水","夹点心","拖地","擦桌子","关闭窗帘","关筒灯","开大厅灯","搬椅子","打开窗帘","关大厅灯","开筒灯"

--- robowaiter/scene/tasks/VLM.py
+++ robowaiter/scene/tasks/VLM.py
@@ -6,34 +6,17 @@
 import time
 from robowaiter.scene.scene import Scene
 
-
 class SceneVLM(Scene):
     def __init__(self, robot):
         super().__init__(robot)
 
     def _reset(self):
-        self.reset_sim()
+        pass
 
-        self.add_walker(1085, 2630, 220)
-        self.control_walker([self.walker_control_generator(0, False, 100, 755, 1900, 180)])
+    def _run(self, op_type=1):
 
-    def _run(self):
-        # 空调操作
-        self.walk_to(950, 1260, 90)  # 没法转向？
-        # todo: 手臂操作
-        time.sleep(5)
-        self.walk_to(947, 1900, 0)
-        
-        # 物品挪动
-        # todo: 视觉导航至目标点，操作手臂至可抓位置
-        """
-        scene.grasp(1, your_objectID)
-        """
-
-        # todo: 视觉导航至目标点，找准释放位置
-        """
-        scene.release(1)
-        """
+        self.move_task_area(op_type)
+        self.op_task_execute(op_type)
 
     def _step(self):
         pass

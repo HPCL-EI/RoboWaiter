1049792d7221e8eee8da5ab440348dc32e0dd9b1
Netceor <45135347+Netceor@users.noreply.github.com>
2023-11-11 14:58:20 +0800

    更新操作任务:官方接口、空调、抓握放置物体

--- robowaiter/scene/tasks/VLM.py
+++ robowaiter/scene/tasks/VLM.py
@@ -17,16 +17,23 @@ class SceneVLM(Scene):
     def _reset(self):
         pass
 
-    def _run(self, op_type=1):
-        # 12个操作顺序测试
-        # for i in range(1,13):
+    def _run(self, op_type=2):
+        # 顺序测试16个操作
+        # for i in range(1,16):
         #     if i<=10:
         #         self.move_task_area(i)
         #     self.op_task_execute(i)
 
-        # if op_type<=10:
+        # 16: 抓操作需要传入物品id，17: 放操作需要传入放置位置周围的空地区域(桌边而不是桌上)
+        # if op_type == 16:
+        #     self.gen_obj()
+        #     self.op_task_execute(op_type, obj_id=0)
+        # # 原始吧台处:[247.0, 520.0, 100.0], 空调开关旁吧台:[240.0, 40.0, 70.0], 水杯桌:[-70.0, 500.0, 107]
+        # # 桌子1:[-55.0, 0.0, 107],桌子1:[-55.0, 150.0, 107]
+        # elif op_type == 17: self.op_task_execute(op_type, release_pos=[-55.0, 150.0, 107])
+        # else:
         #     self.move_task_area(op_type)
-        # self.op_task_execute(op_type)
+        #     self.op_task_execute(op_type)
         pass
     
     def _step(self):

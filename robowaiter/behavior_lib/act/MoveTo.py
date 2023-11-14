da224f745d03903ec104a21a3b8fca23c4926e90
Caiyishuai <39987654+Caiyishuai@users.noreply.github.com>
2023-11-14 12:09:53 +0800

    更新了部分动作节点

--- robowaiter/behavior_lib/act/MoveTo.py
+++ robowaiter/behavior_lib/act/MoveTo.py
@@ -14,23 +14,39 @@ class MoveTo(Act):
 
 
     @classmethod
-    def get_info(self,arg):
+    def get_info(cls,arg):
         info = {}
+        info['pre'] = set()
         info["add"] = {f'At(Robot,{arg})'}
-        info["del"] = {f'At(Robot,{place})' for place in self.valid_args if place != arg}
+        info["del_set"] = {f'At(Robot,{place})' for place in cls.valid_args if place != arg}
         return info
 
 
     def _update(self) -> ptree.common.Status:
         # self.scene.test_move()
 
-        navigator = Navigator(scene=self.scene, area_range=[-350, 600, -400, 1450], map=self.scene.state["map"]["2d"])
-        goal = self.scene.state['map']['obj_pos'][self.args[0]]
-        navigator.navigate(goal, animation=False)
+        # navigator = Navigator(scene=self.scene, area_range=[-350, 600, -400, 1450], map=self.scene.state["map"]["2d"])
+        # goal = self.scene.state['map']['obj_pos'][self.args[0]]
+        # navigator.navigate(goal, animation=False)
 
-        self.scene.state['condition_set'].add('At(Robot,Table)')
+        if self.target_place in Act.place_xyz_dic:
+            goal = Act.place_xyz_dic[self.target_place]
+            self.scene.walk_to(goal[0],goal[1])
+        else:
+            # 获取obj_id
+            for id,obj in enumerate(self.scene.objects):
+                if obj.name == self.target_place:
+                    obj_id = id
+                    break
+
+            obj_info = self.scene.objects[obj_id]
+            obj_x, obj_y, obj_z = obj_info.location.X, obj_info.location.Y, obj_info.location.Z
+            self.scene.walk_to(obj_x,obj_y)
 
         # goal = self.scene.state['map']['obj_pos'][self.args[0]]
-        # self.scene.walk_to(goal[0],goal[1])
+        # self.scene.walk_to(goal[0],goal[1]) # X, Y, Yaw=None, velocity=200, dis_limit=0
 
+
+        self.scene.state["condition_set"] |= (self.info["add"])
+        self.scene.state["condition_set"] -= self.info["del_set"]
         return ptree.common.Status.RUNNING
--- robowaiter/behavior_lib/cond/Is.py
+++ robowaiter/behavior_lib/cond/Is.py
@@ -3,12 +3,19 @@ from typing import Any
 from robowaiter.behavior_lib._base.Cond import Cond
 import itertools
 
-class At(Cond):
+class Is(Cond):
     can_be_expanded = True
     num_params = 2
+    valid_params1 = [('AC','TubeLight','HallLight','Curtain'),
+                    ('On','Off')]
+    valid_params2 = [('Table1','Floor','Chairs'),
+                    ('Clean','Dirty')]
+    valid_params3 = [('ACTemperature'),
+                    ('Up','Down')]
 
-    valid_args = list(itertools.product(('Robot','Customer'), tuple(Cond.all_object | Cond.all_place | {'Customer'})))
-    valid_args.remove(('Customer','Customer'))
+    valid_args = list(itertools.product(valid_params1[0], valid_params1[1]))
+    valid_args.extend(list(itertools.product(valid_params2[0], valid_params2[1])))
+    valid_args.extend(list(itertools.product(valid_params3[0], valid_params3[1])))
     valid_args = tuple(valid_args)
 
     def __init__(self,*args):
